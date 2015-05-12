05d88b531ef1e5ea862d6ff583d9275943e6f698
zhaoda(赵达) <zd6437@gmail.com>
2015-05-13 00:25:48 +0800

    去掉“hack在微信等webview中无法修改document.title的情况”

--- dist/spa.js
+++ dist/spa.js
@@ -1916,11 +1916,11 @@
 
     title && (document.title = title)
     // hack在微信等webview中无法修改document.title的情况
-    var $iframe = $('<iframe src="/favicon.ico"></iframe>').on('load', function() {
-      setTimeout(function() {
-        $iframe.off('load').remove()
-      }, 0)
-    }).appendTo($body)
+    // var $iframe = $('<iframe src="/favicon.ico"></iframe>').on('load', function() {
+    //   setTimeout(function() {
+    //     $iframe.off('load').remove()
+    //   }, 0)
+    // }).appendTo($body)
 
     hash = url + '#' + hash
     
